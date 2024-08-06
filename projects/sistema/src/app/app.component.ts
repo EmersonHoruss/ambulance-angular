bb6d0a08e2e09872c5ee6c2a7b03dd3a6f72d84c
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2024-08-06 17:12:40 -0500

    created header and sidebar

--- projects/sistema/src/app/app.component.ts
+++ projects/sistema/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CoreModule } from './core/core.module';
 
 @Component({
   selector: 'amb-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CoreModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- projects/sistema/src/app/core/core.module.ts
+++ projects/sistema/src/app/core/core.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HeaderComponent } from './views/components/header/header.component';
+import { SidebarComponent } from './views/components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, HeaderComponent, SidebarComponent],
+  exports: [HeaderComponent, SidebarComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
