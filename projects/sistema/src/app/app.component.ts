f0dbf9eba66fe4bdcaab36712ce58c66a54540b7
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2024-08-06 23:28:34 -0500

    header: menu and user buttons

--- projects/sistema/src/app/app.component.ts
+++ projects/sistema/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CoreModule } from './core/core.module';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'amb-root',
   standalone: true,
-  imports: [RouterOutlet, CoreModule],
+  imports: [RouterOutlet, CoreModule, MatSidenavModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'sistema';
+  isOpened = true;
 }
--- projects/sistema/src/app/core/views/components/header/header.component.ts
+++ projects/sistema/src/app/core/views/components/header/header.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'amb-header',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatMenuModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  @Output() onToggleMenu: EventEmitter<void> = new EventEmitter();
+  userName = 'David Perales';
 
+  toggleMenu() {
+    this.onToggleMenu.emit();
+  }
 }
