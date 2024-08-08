f31f2627f31a416b497a3dea0c4a67c2ad5f4ecb
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2024-08-08 01:43:23 -0500

    page-list and form components for driver, medic and user modules

--- projects/sistema/src/app/medic/medic.module.ts
+++ projects/sistema/src/app/medic/medic.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MedicRoutingModule } from './medic-routing.module';
-
+import { FormComponent } from './views/components/form/form.component';
+import { PageListComponent } from './views/pages/page-list/page-list.component';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    MedicRoutingModule
-  ]
+  imports: [CommonModule, MedicRoutingModule, FormComponent, PageListComponent],
 })
-export class MedicModule { }
+export class MedicModule {}
--- projects/sistema/src/app/user/user.module.ts
+++ projects/sistema/src/app/user/user.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
-
+import { PageListComponent } from './views/pages/page-list/page-list.component';
+import { FormComponent } from './views/components/form/form.component';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    UserRoutingModule
-  ]
+  imports: [CommonModule, UserRoutingModule, FormComponent, PageListComponent],
 })
-export class UserModule { }
+export class UserModule {}
