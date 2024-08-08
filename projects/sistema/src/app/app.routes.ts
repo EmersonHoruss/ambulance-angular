35971f7b59b5820bac6907110ed7a60b00fd21a6
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2024-08-08 01:46:15 -0500

    lazy loading for user, driver, medic and history

--- projects/sistema/src/app/app.routes.ts
+++ projects/sistema/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'user',
+    loadChildren: () =>
+      import('./user/user.module').then((module) => module.UserModule),
+  },
+  {
+    path: 'driver',
+    loadChildren: () =>
+      import('./driver/driver.module').then((module) => module.DriverModule),
+  },
+  {
+    path: 'medic',
+    loadChildren: () =>
+      import('./medic/medic.module').then((module) => module.MedicModule),
+  },
+  {
+    path: 'history',
+    loadChildren: () =>
+      import('./history/history.module').then((module) => module.HistoryModule),
+  },
+];
