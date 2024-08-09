894fc13146feea53731122327cea12402f292639
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2024-08-09 00:41:42 -0500

    container component and set in page list of each module: driver, history, user and medic

--- projects/sistema/src/app/shared/shared.module.ts
+++ projects/sistema/src/app/shared/shared.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 import { TitleComponent } from './components/title/title.component';
+import { ContainerComponent } from './components/container/container.component';
 
 @NgModule({
-  declarations: [TitleComponent],
-  imports: [CommonModule, MatIconModule],
-  exports: [TitleComponent, MatIconModule, MatButtonModule],
+  declarations: [TitleComponent, ContainerComponent],
+  imports: [CommonModule, MatIconModule, MatCardModule],
+  exports: [TitleComponent, MatIconModule, MatButtonModule, ContainerComponent],
 })
 export class SharedModule {}
