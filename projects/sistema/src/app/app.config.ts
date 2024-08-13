ef34f90b9208f3fc27d2bdd6a69f0afb3016832e
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2024-08-13 00:22:03 -0500

    customized labels and text of paginator

--- projects/sistema/src/app/app.config.ts
+++ projects/sistema/src/app/app.config.ts
@@ -4,6 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { MatPaginatorIntl } from '@angular/material/paginator';
+import { PaginatorService } from './shared/services/paginator.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,5 +13,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideAnimationsAsync(),
     provideHttpClient(),
+    { provide: MatPaginatorIntl, useClass: PaginatorService },
   ],
 };
