0d07d1bb3ba53673be25480f62b991d2aca0c1c2
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2024-08-13 00:08:03 -0500

    paginator component

--- projects/sistema/src/app/shared/shared.module.ts
+++ projects/sistema/src/app/shared/shared.module.ts
@@ -5,20 +5,34 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 import { TitleComponent } from './components/title/title.component';
 import { ContainerComponent } from './components/container/container.component';
 import { TableComponent } from './components/table/table.component';
+import { PaginatorComponent } from './components/paginator/paginator.component';
 
 @NgModule({
-  declarations: [TitleComponent, ContainerComponent, TableComponent],
-  imports: [CommonModule, MatIconModule, MatCardModule, MatTableModule],
+  declarations: [
+    TitleComponent,
+    ContainerComponent,
+    TableComponent,
+    PaginatorComponent,
+  ],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatCardModule,
+    MatTableModule,
+    MatPaginatorModule,
+  ],
   exports: [
     TitleComponent,
     MatIconModule,
     MatButtonModule,
     ContainerComponent,
     TableComponent,
+    PaginatorComponent,
   ],
 })
 export class SharedModule {}
